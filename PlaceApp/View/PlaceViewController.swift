fa333f4fbe5cc9282a3055e2a5c8a1daf5e27b5e
Damagucci-Juice <jsr9874@gmail.com>
2026-01-14 08:30:53 +0900

    feat: 로딩용 뷰와 프로그레스 바 추가

--- PlaceApp/View/PlaceViewController.swift
+++ PlaceApp/View/PlaceViewController.swift
@@ -14,6 +14,24 @@ final class PlaceViewController: UIViewController {
     @IBOutlet var filterSegmentControl: UISegmentedControl!
     @IBOutlet var tableView: UITableView!
 
+    private lazy var loadingView: UIView = {
+        let result = UIView()
+        result.backgroundColor = .systemGray6
+        return result
+    }()
+
+    private lazy var progressView: UIProgressView = {
+        let view = UIProgressView()
+        view.trackTintColor = .lightGray
+        view.progressTintColor = .systemBlue
+        view.progress = 0.1
+        return view
+    }()
+
+    private var time: Float = 0.0
+    private var timer: Timer?
+
+
     private var region: Region = .all {
         didSet {
             tableView.reloadData()
@@ -29,7 +47,40 @@ final class PlaceViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupLayout()
         setupAttributes()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+            guard let self else { return }
+            self.didTapDownloadButton()
+        }
+    }
+
+    private func setupLayout() {
+        view.addSubview(loadingView)
+
+        loadingView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            loadingView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            loadingView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            loadingView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            loadingView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+        ])
+
+        loadingView.addSubview(progressView)
+
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            progressView.topAnchor.constraint(equalTo: loadingView.topAnchor),
+            progressView.leadingAnchor.constraint(equalTo: loadingView.leadingAnchor),
+            progressView.trailingAnchor.constraint(equalTo: loadingView.trailingAnchor)
+        ])
     }
 
     private func setupAttributes() {
@@ -42,6 +93,27 @@ final class PlaceViewController: UIViewController {
 }
 
 private extension PlaceViewController {
+    @objc private func didTapDownloadButton() {
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(
+            timeInterval: 0.05,
+            target: self,
+            selector: #selector(setProgress),
+            userInfo: nil,
+            repeats: true
+        )
+    }
+
+    @objc private func setProgress() {
+        time += 0.05
+        progressView.setProgress(time, animated: true)
+        if time >= 1.0 {
+            // 완료 액션
+            self.loadingView.isHidden = true
+            timer?.invalidate()
+        }
+    }
+
     func setTextField() {
         [UIControl.Event.editingDidEndOnExit, .editingChanged].forEach {
             searchTextField.addTarget(self, action: #selector(showSearchResult), for: $0)
