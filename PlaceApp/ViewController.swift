d5d75ef9128721db49dd0d3d78e368c63346a17c
Damagucci-Juice <jsr9874@gmail.com>
2026-01-13 15:47:33 +0900

    Initial Commit

---
+++ PlaceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlaceApp
+//
+//  Created by Gucci on 1/13/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
