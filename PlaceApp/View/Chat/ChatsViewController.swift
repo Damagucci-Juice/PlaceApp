d5bda42f8c1bdc9db8aca6bdb5ad009e8a05727c
Damagucci-Juice <jsr9874@gmail.com>
2026-01-18 12:33:36 +0900

    feat: 채팅 목록 서치바 구현

--- PlaceApp/View/Chat/ChatsViewController.swift
+++ PlaceApp/View/Chat/ChatsViewController.swift
@@ -11,40 +11,39 @@ final class ChatsViewController: UIViewController, Reusable {
 
     @IBOutlet var tableView: UITableView!
 
+    @IBOutlet var searchTextField: UITextField!
+
+    var datasource = [ChatRoom]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
         setupAttribute()
+        datasource = mockChatRooms
     }
 
 }
 
+// MARK: - Tableview
 
 extension ChatsViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(#function, indexPath)
-
-        // 1. 어떤 스토리보드 파일
         let sb = UIStoryboard(name: "Main", bundle: nil)
-        // 2. 어떤 뷰 컨트롤러
         let vc = sb.instantiateViewController(withIdentifier: DialogViewController.identifier) as! DialogViewController
-
-        vc.chatroom = mockChatRooms[indexPath.row]
-
-        // 3. 어떤 방식으로 띄워줘
+        vc.chatroom = datasource[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
 }
 
 extension ChatsViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        mockChatRooms.count
+        datasource.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: PersonalChatRoomTableViewCell.identifier, for: indexPath) as? PersonalChatRoomTableViewCell else { return UITableViewCell() }
 
-        cell.configure(mockChatRooms[indexPath.row])
+        cell.configure(datasource[indexPath.row])
 
         cell.selectionStyle = .none
 
@@ -67,6 +66,9 @@ extension ChatsViewController: TableBasicProtocol {
     }
 }
 
+
+// MARK: - View Controllers
+
 extension ChatsViewController: Drawable {
     // storyboard constraints
     func setupUI() { }
@@ -74,9 +76,56 @@ extension ChatsViewController: Drawable {
     func setupAttribute() {
         setupTable()
         setupNaviItem()
+        setupTextField()
     }
     
     func setupNaviItem() {
         navigationItem.title = "TRAVEL TALK"
+    }
+}
+
+// MARK: - SearchTextField
+
+extension ChatsViewController {
+
+    func setupTextField() {
+        searchTextField.setDefaultStyle("상대방 검색")
+        searchTextField.addTarget(
+            self,
+            action: #selector(textFieldEditingDidEndOnExit),
+            for: .editingDidEndOnExit
+        )
+    }
+
+    @objc private func textFieldEditingDidEndOnExit(_ tf: UITextField) {
+        print(#function, )
+        guard let text = tf.text, !text.isEmpty else {
+            setDatasource(mockChatRooms)
+            return
+        }
+        
+        setDatasource(chatRooms(by: filteredUsers(text)))
+    }
+    
+    private func setDatasource(_ roominfo: [ChatRoom]) {
+        datasource = roominfo
+        tableView.reloadData()
+    }
+    
+    private func filteredUsers(_ text: String) -> [User] {
+        mockUsers.filter { user in
+            user.userName.lowercased().contains(text.lowercased())
+        }
+    }
+    
+    private func chatRooms(by users: [User]) -> [ChatRoom] {
+        var result = [ChatRoom]()
+        for user in users {
+            let chrooms = mockChatRooms.filter { cr in
+                cr.participantIds.contains(user.userId)
+            }
+            result.append(contentsOf: chrooms)
+        }
+        return result
     }
 }
