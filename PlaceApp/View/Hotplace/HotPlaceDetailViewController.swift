8d3e4df4c4f2e63c9f7e9b23fae46332e6988587
Damagucci-Juice <jsr9874@gmail.com>
2026-01-19 16:54:54 +0900

    feat: 핫플레이스 명소 뷰컨트롤러 구현 완료

--- PlaceApp/View/Hotplace/HotPlaceDetailViewController.swift
+++ PlaceApp/View/Hotplace/HotPlaceDetailViewController.swift
@@ -23,22 +23,86 @@ final class HotPlaceDetailViewController: UIViewController {
         result.backgroundColor = .white
         result.setCorner(16)
         result.axis = .vertical
-        result.alignment = .leading
+        result.alignment = .fill
         result.spacing = 8.0
-        result.distribution = .equalSpacing
+        result.distribution = .fill
+
+        result.isLayoutMarginsRelativeArrangement = true
+        result.layoutMargins = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
         return result
     }()
+
+    private let interSpacing = 8.0
 
     private lazy var spotNameStackView = {
         let result = UIStackView()
         result.axis = .horizontal
-        result.alignment = .leading
+        result.alignment = .center
         result.distribution = .fillProportionally
-        result.spacing = 4
+        result.spacing = interSpacing
 
-        
+        result.addArrangedSubview(makeImage("network"))
+        result.addArrangedSubview(makeLabel(tour.englishName))
+        return result
+    }()
 
+    private lazy var cityNameStackView = {
+        let result = UIStackView()
+        result.axis = .horizontal
+        result.alignment = .center
+        result.distribution = .fillProportionally
+        result.spacing = interSpacing
 
+        result.addArrangedSubview(makeImage("info.circle"))
+        result.addArrangedSubview(makeLabel(tour.city))
+        return result
+    }()
+
+    private lazy var mapStackView = {
+        let result = UIStackView()
+        result.axis = .horizontal
+        result.alignment = .center
+        result.distribution = .fillProportionally
+        result.spacing = interSpacing
+
+        result.addArrangedSubview(makeImage("map"))
+        result.addArrangedSubview(makeLabel(tour.address))
+        return result
+    }()
+
+    private lazy var phoneStackView = {
+        let result = UIStackView()
+        result.axis = .horizontal
+        result.alignment = .center
+        result.distribution = .fillProportionally
+        result.spacing = interSpacing
+
+        result.addArrangedSubview(makeImage("phone.fill"))
+        result.addArrangedSubview(makeLabel(tour.phoneNumber))
+        return result
+    }()
+
+    private lazy var urlStackView = {
+        let result = UIStackView()
+        result.axis = .horizontal
+        result.alignment = .center
+        result.distribution = .fillProportionally
+        result.spacing = interSpacing
+
+        result.addArrangedSubview(makeImage("safari"))
+        result.addArrangedSubview(makeLabel(tour.websiteURL))
+        return result
+    }()
+
+    private lazy var coordinationStackView = {
+        let result = UIStackView()
+        result.axis = .horizontal
+        result.alignment = .center
+        result.distribution = .fillProportionally
+        result.spacing = interSpacing
+
+        result.addArrangedSubview(makeImage("paperplane.circle"))
+        result.addArrangedSubview(makeLabel("\(tour.latitude), \(tour.longitude)"))
         return result
     }()
 
@@ -60,43 +124,86 @@ final class HotPlaceDetailViewController: UIViewController {
         setupAttribute()
         setupNaviItem()
     }
+
+    private func makeLabel(_ title: String) -> UILabel {
+        let label = UILabel()
+        label.text = title
+        label.font = .systemFont(ofSize: 16)
+        label.textColor = .black
+        label.numberOfLines = 0
+        label.textAlignment = .left
+        return label
+    }
+
+    private func makeImage(_ image: String) -> UIImageView {
+        let image = UIImage(systemName: image)
+        let result = UIImageView(image: image)
+        result.contentMode = .scaleAspectFit
+        result.tintColor = .tintColor
+        result.frame = .init(origin: .zero,
+                             size: .init(width: 20, height: 20))
+        return result
+    }
 }
 
 extension HotPlaceDetailViewController: Drawable {
     func setupUI() {
         view.addSubview(mapView)
+        view.addSubview(infoStackView)
 
         mapView.snp.makeConstraints { make in
             make.top.leading.equalTo(view.safeAreaLayoutGuide).offset(20)
             make.trailing.equalTo(view.safeAreaLayoutGuide).offset(-20)
-            make.height.equalToSuperview().dividedBy(4)
+            make.height.equalToSuperview().dividedBy(3)
+        }
+
+        infoStackView.snp.makeConstraints { make in
+            make.top.equalTo(mapView.snp.bottom).offset(16)
+            make.horizontalEdges.equalTo(mapView)
         }
     }
 
     func setupAttribute() {
+        view.backgroundColor = .secondarySystemBackground
         setMapView()
+        setupInfoViews()
     }
 
-    func setMapView() {
-        let targetCoordinate = CLLocationCoordinate2D(latitude: tour.latitude, longitude: tour.longitude) // San Francisco
 
+    func setupNaviItem() {
+        navigationItem.title = tour.koreanName
+    }
+
+    private func setupInfoViews() {
+        [
+            spotNameStackView,
+            cityNameStackView,
+            mapStackView,
+            phoneStackView,
+            urlStackView,
+            coordinationStackView
+        ].forEach { stack in
+            infoStackView.addArrangedSubview(stack)
+        }
+    }
+    
+    private func setMapView() {
+        let targetCoordinate = CLLocationCoordinate2D(latitude: tour.latitude, longitude: tour.longitude) // San Francisco
+        
         // Define the visible area (zoom level)
         let region = MKCoordinateRegion(
             center: targetCoordinate,
-            span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05) // Adjust span for zoom
+            span: MKCoordinateSpan(latitudeDelta: 0.03, longitudeDelta: 0.03) // Adjust span for zoom
         )
-
+        
         let annotation = MKPointAnnotation()
         annotation.coordinate = targetCoordinate
         annotation.title = tour.koreanName
         annotation.subtitle = tour.city
-
+        
         mapView.setRegion(region, animated: true)
         mapView.addAnnotation(annotation)
-    }
-
-    func setupNaviItem() {
-        navigationItem.title = tour.koreanName
+        mapView.setCorner(16)
     }
 }
 
